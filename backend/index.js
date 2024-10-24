4224e0090357f6cee13d0da61036c714934b7d8e
baisalikhan <140602823+Baisexodevs@users.noreply.github.com>
2024-10-24 22:52:00 +0500

    basic setup of the project

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import mysql from "mysql2";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "12345",
+  database: "test",
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("Hello from backend");
+});
+
+app.get("/books", (req, res) => {
+  const query = "SELECT * FROM books";
+  db.query(query, (err, data) => {
+    if (err) {
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const query = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)";
+  const values = [req.body.title, req.body.desc, req.body.cover];
+
+  db.query(query, [values], (err, data) => {
+    if (err) {
+      return res.json(err);
+    }
+    return res.json("Book has been created successfully");
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Connected to backend");
+});
---
+++ backend/readme.txt
@@ -0,0 +1,20 @@
+1. npm init -y
+2. npm i express mysql nodemon
+3. created index.js (file name can vary)
+
+4. initial code/app:
+    import express from "express";
+    const app = express();
+
+    app.listen(8800, () => {
+    console.log("Connected to backend");
+    });
+
+5. node index.js (file_name) to start the server
+
+6. Add the following line in the package.js
+
+    6.1 "type": "module", (the error on 'import line in header' will end)
+
+    6.2 Add the following line in the package.js > scripts
+        "start": "nodemon index.js"
